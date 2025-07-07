f1fcb5398f7ddb83e53fb7da6b830c98c703d142
Jason Schuchardt <jargon.schuchardt@gmail.com>
2025-07-07 13:26:58 -0700

    fixed merge issues

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-/*
- * main.js
- * Copyright (C) 2019 jasonsch <jasonsch@walnut>
- *
- * Distributed under terms of the MIT license.
- */
-/*
-(function(){
-  'use strict';
-  
-    
-})();*/
-
-$(document).ready(function() {
-  $("span.my_email").each(function(index) {
-    this.innerHTML = '<a href="mailto:jason.sch@math.ucla.edu">jason.sch@math.ucla.edu</a>';
-  });
-});
-
-
-
-
-
-/*
- *
- * sdfjaskdf;laskdf
- *
- *
- * kaskdfla
- * */
-=======
 /*
  * main.js
  * Copyright (C) 2019 jasonsch <jasonsch@walnut>
@@ -57,7 +25,4 @@ $(document).ready(function() {
  *
  * sdfjaskdf;laskdf
  *
- *
- * kaskdfla
  * */
->>>>>>> 01791c6f6c57f1065bb2c7eba1d9169e3829aed4
--- current-site/assets/js/main.js
+++ current-site/assets/js/main.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-/*
- * main.js
- * Copyright (C) 2019 jasonsch <jasonsch@walnut>
- *
- * Distributed under terms of the MIT license.
- */
-/*
-(function(){
-  'use strict';
-  
-    
-})();*/
-
-$(document).ready(function() {
-  $("span.my_email").each(function(index) {
-    this.innerHTML = '<a href="mailto:jason.sch@math.ucla.edu">jason.sch@math.ucla.edu</a>';
-  });
-});
-
-
-
-
-
-/*
- *
- * sdfjaskdf;laskdf
- *
- *
- * kaskdfla
- * */
-=======
 /*
  * main.js
  * Copyright (C) 2019 jasonsch <jasonsch@walnut>
@@ -60,4 +28,3 @@ $(document).ready(function() {
  *
  * kaskdfla
  * */
->>>>>>> 01791c6f6c57f1065bb2c7eba1d9169e3829aed4
